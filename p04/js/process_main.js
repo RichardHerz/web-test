89971eb0fb1b029bc6dc9b0fdfcb5431ecf10019
RichardHerz <rich@reactorlab.net>
2018-02-21 21:09:08 -0800

    y

--- p04/js/process_main.js
+++ p04/js/process_main.js
@@ -28,7 +28,7 @@ var viewport_meta = document.getElementById('viewport-meta');
 // Define our viewport meta values
 var viewports = {
 		default: viewport_meta.getAttribute('content'),
-		landscape: 'width=990'
+		landscape: 'initial-scale=0.66'
 	};
 
 // Change the viewport value based on screen.width
