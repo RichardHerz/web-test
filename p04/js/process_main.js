faacc832a179c42043b5f05783d7610428645b2e
RichardHerz <rich@reactorlab.net>
2018-02-21 20:59:04 -0800

    try javascript to change viewport

--- p04/js/process_main.js
+++ p04/js/process_main.js
@@ -22,6 +22,31 @@
     simParams.updateRunCount();
   } // END OF function openThisLab
 
+  // Store the meta element
+var viewport_meta = document.getElementById('viewport-meta');
+
+// Define our viewport meta values
+var viewports = {
+		default: viewport_meta.getAttribute('content'),
+		landscape: 'width=990'
+	};
+
+// Change the viewport value based on screen.width
+var viewport_set = function() {
+		if ( screen.width > 768 )
+			viewport_meta.setAttribute( 'content', viewports.landscape );
+		else
+			viewport_meta.setAttribute( 'content', viewports.default );
+	}
+
+// Set the correct viewport value on page load
+viewport_set();
+
+// Set the correct viewport after device orientation change or resize
+window.onresize = function() {
+	viewport_set();
+}
+
   function runSimulation() {
 
     // CALLED BY function runThisLab ON CLICK OF RUN-PAUSE BUTTON
