4ca6525b07ff4a8d5ba7e98da4ca44a956039815
RichardHerz <rich@reactorlab.net>
2018-03-02 17:47:30 -0800

    work on new reactor

--- p05/js/reactor.js
+++ p05/js/reactor.js
@@ -11,15 +11,24 @@ window.onload = openThisLab; // can NOT use = openThisLab();
 function openThisLab() {
 
   var el = document.querySelector("#div_PLOTDIV_reactor_contents");
-  el.style.top = "156px";
-  el.style.height = "43px";
+  el.style.top = "198px";
+  el.style.height = "2px";
 
 } // END OF function openThisLab
 
 var updateDisplayTimingMs = 100;
-var flag = 0;
+var fillFlag = 0;
+var emptyFlag = 0;
+var reactFlag = 0;
 
 function fillReactor() {
+
+  if (emptyFlag == 1 || reactFlag == 1) {
+    fillFlag = 0;
+    return;
+  } else {
+    fillFlag = 1;
+  }
 
   // get time at start of repeating fillReactor
   startDate = new Date(); // need this here
@@ -28,32 +37,36 @@ function fillReactor() {
   // both two lines below work by themselves and don't require jquery
   var el = document.querySelector("#div_PLOTDIV_reactor_contents");
   // var el = document.getElementById("div_PLOTDIV_reactor_contents");
+  var top = parseFloat(el.style.top); // convert, e.g., "100px" to 100
+  var height = parseFloat(el.style.height); // convert, e.g., "100px" to 100
 
-    var top = parseFloat(el.style.top); // convert, e.g., "100px" to 100
-    var height = parseFloat(el.style.height); // convert, e.g., "100px" to 100
-    el.style.top = top - 2 + 'px';
-    height = height + 2;
-    el.style.height = height + 'px';
+  // put this before change height or get height change each onclick
+  if (height >= 70) {
+    fillFlag = 0;
+    return;
+  }
 
-    if (height >= 200) {flag = 1;}
+  el.style.top = top - 2 + 'px';
+  height = height + 2;
+  el.style.height = height + 'px';
 
-  // >>> BREAK OUT fillReactor WHEN REACTOR FULL
-
+  // CONTINUE fillReactor WITH CALL TO ITSELF AFTER updateMs WAIT
   var thisDate = new Date();
   var currentMs = thisDate.getTime();
   elapsedMs = currentMs - startMs;
   updateMs = updateDisplayTimingMs - elapsedMs;
-
-  if (flag == 0) {
-  // END fillReactor WITH CALL TO ITSELF AFTER updateMs WAIT
-  setTimeout(fillReactor, updateMs);  // updateMs
-} else {
-  flag = 0; // set flag back to zero for other actions
-}
+  setTimeout(fillReactor, updateMs);  // fillReactor, updateMs
 
 } // END OF function fillReactor
 
 function emptyReactor() {
+
+  if (fillFlag == 1 || reactFlag == 1) {
+    emptyFlag = 0;
+    return;
+  } else {
+    emptyFlag = 1;
+  }
 
   // get time at start of repeating emptyReactor
   startDate = new Date(); // need this here
@@ -62,31 +75,62 @@ function emptyReactor() {
   // both two lines below work by themselves and don't require jquery
   var el = document.querySelector("#div_PLOTDIV_reactor_contents");
   // var el = document.getElementById("div_PLOTDIV_reactor_contents");
+  var top = parseFloat(el.style.top); // convert, e.g., "100px" to 100
+  var height = parseFloat(el.style.height); // convert, e.g., "100px" to 100
 
-    var top = parseFloat(el.style.top); // convert, e.g., "100px" to 100
-    var height = parseFloat(el.style.height); // convert, e.g., "100px" to 100
-    el.style.top = top + 2 + 'px';
-    height = height - 2;
-    el.style.height = height + 'px';
+  // put this before change height or get height change each onclick
+  if (height <= 2) {
+    emptyFlag = 0;
+    return;
+  }
 
-    if (height <= 1) {flag = 1;}
+  el.style.top = top + 2 + 'px';
+  height = height - 2;
+  el.style.height = height + 'px';
 
-  // >>> BREAK OUT emptyReactor WHEN REACTOR EMPTY
-
+  // CONTINUE emptyReactor WITH CALL TO ITSELF AFTER updateMs WAIT
   var thisDate = new Date();
   var currentMs = thisDate.getTime();
   elapsedMs = currentMs - startMs;
   updateMs = updateDisplayTimingMs - elapsedMs;
-
-  if (flag == 0) {
-  // END emptyReactor WITH CALL TO ITSELF AFTER updateMs WAIT
-  setTimeout(emptyReactor, updateMs);  // updateMs
-} else {
-  flag = 0; // set flag back to zero for other actions
-}
+  setTimeout(emptyReactor, updateMs);  // emptyReactor, updateMs
 
 } // END OF function emptyReactor
 
 function reactReactor() {
+
+  if (fillFlag == 1 || emptyFlag == 1) {
+    reactFlag = 0;
+    return;
+  } else {
+    reactFlag = 1;
+  }
+
+  // get time at start of repeating reactReactor
+  startDate = new Date(); // need this here
+  startMs = startDate.getTime();
+
+  // both two lines below work by themselves and don't require jquery
+  var el = document.querySelector("#div_PLOTDIV_reactor_contents");
+  // var el = document.getElementById("div_PLOTDIV_reactor_contents");
+
+  // >>> BREAK OUT reactReactor WHEN REACTION DONE
+  // put this before change reaction or get reaction change each onclick
+  // if (height <= 2) {
+  //   emptyFlag = 0;
+  //   return;
+  // }
+
+  el.style.backgroundColor = "Tomato"; // backgroundColor NOT background-color
+  reactFlag = 0;
+
+  return; // xxx TEMPORARY
+
+  // CONTINUE reactReactor WITH CALL TO ITSELF AFTER updateMs WAIT
+  var thisDate = new Date();
+  var currentMs = thisDate.getTime();
+  elapsedMs = currentMs - startMs;
+  updateMs = updateDisplayTimingMs - elapsedMs;
+  setTimeout(reactReactor, updateMs);  // reactReactor, updateMs
 
 } // END OF function reactReactor
