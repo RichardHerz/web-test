a3f735ee549e21ac980110b98f12084344e7af56
RichardHerz <rich@reactorlab.net>
2018-03-27 07:35:12 -0700

    work on p06 heat exchanger
    replace JS with that from reaction-diffusion and start editing

---
+++ p06/dev/README.md
@@ -0,0 +1,20 @@
+# lcCardLayoutToWeb
+This goal of this project is a design tool for one-off web simulations in which page elements are placed in non-grid layouts. The project is not intended to build full-featured web pages. There is no LiveCode script conversion.
+
+You can design a layout in LiveCode, then export the layout to HTML and CSS files. Drag and drop design!
+
+The project is "reversible" - an exported web page, which has changes such as addition of file and event function references, can be imported back into to the project, edited (controls moved, added and deleted), and then exported back to a revised web page with the file and event references preserved.
+
+![parakeet still 20161006](http://reactorlab.net/graphics/github_media/parakeet_20161007a.png)
+
+This project is being built with the open-source, Community Edition of LiveCode. Get it at www.LiveCode.org. You need to use the LiveCode IDE, version 8 or higher, in order to use this project.
+
+My use of this project is to develop web apps that are interactive simulations of physical systems. Examples of web apps built with this project are posted at http://reactorlab.net/resources/web-app-experiments/ 
+
+Each LiveCode control is referenced in CSS by id only, thus cascading is limited. LiveCode controls supported include groups, standard buttons, menu buttons, checkboxes, radio buttons, locked fields, unlocked fields, and scrollbars (as range inputs). See the project wiki for other limitations.
+
+The css-processor stack can be used to process CSS files in order to scale a card layout to other widths.
+
+SEE THIS PROJECT'S WIKI IN THE TAB ABOVE FOR MORE INFO. 
+
+CONTACT INFO FOR Richard Herz IS AT www.ReactorLab.net ( Richard K. Herz ) 
---
+++ p06/js/process_spacetime.js
@@ -0,0 +1,145 @@
+/*
+  Design, text, images and code by Richard K. Herz, 2017-2018
+  Copyrights held by Richard K. Herz
+  Licensed for use under the GNU General Public License v3.0
+  https://www.gnu.org/licenses/gpl-3.0.en.html
+*/
+
+function initSpaceTimeArray(numVars,numTimePts,numSpacePts) {
+  // returns 3D array to hold data for multiple variables for SPACE-TIME plots
+  // returns array with all elements for plot filled with zero
+  //    index 1 specifies the variable [0 to numVars-1]
+  //    index 2 specifies the time point [0 to & including numTimePoints]
+  //    index 3 specifies the space point [0 to & including numSpacePoints]
+  //    the element value at plotDataStub[v][t][s] will be the conc or rate
+  //      to be shown for that variable at that time at that space location
+  var v;
+  var s;
+  var t;
+  var plotDataStub = new Array();
+  for (v = 0; v < numVars; v += 1) {
+    plotDataStub[v] = new Array();
+      for (t = 0; t <= numTimePts; t += 1) { // NOTE = AT t <=
+      plotDataStub[v][t] = new Array();
+      for (s = 0; s <= numSpacePts; s += 1) { // NOTE = AT s <=
+        plotDataStub[v][t][s] = 0;
+      }
+    }
+  }
+  // document.getElementById("dev01").innerHTML = "hello";
+  return plotDataStub;
+} // end function initSpaceTimeArray
+
+// create array to hold space-time plot data
+// these become global vars used in other script files
+var numSpaceTimeVars = 1;
+var numTimePts = numStripPts; // numStripPts defined in process_plot_info.js
+var numSpacePts = puCatalystLayer.numNodes;
+// numTimePts should equal numPlotPts on strip chart plots
+// numSpacePts should equal puCatalystLayer.numNodes
+// see file process_plot_info.js
+// if want square canvas 'pixels' set time/space pt ratio = canvas width/height ratio
+var spaceTimeData = initSpaceTimeArray(numSpaceTimeVars,numTimePts,numSpacePts);
+
+function jetColorMap(n) {
+  // input n should be value between 0 and 1
+  // rgb output array values will be 0-255 to match MATLAB's jet colormap
+  //
+  // ANOTHER WAY would be a look up table - would that be faster?
+  //
+  var r;
+  var g;
+  var b;
+  if (n<0) {n = 0;}
+  if (n>1) {n = 1;}
+  // would not need to round input to integers with IF statements
+  // EXCEPT if don't round then can get rgb values > 1.0 at end of IF
+  // so would then need to add more IF statements to check - SO ROUND NOW
+  var n64 = Math.round(1 + 63*n); // n64 = 1 when n = 0; n64 = 64 when n = 1
+  if (n64 >= 1 && n64 < 9) {
+    r = 0;
+    g = 0;
+    b = (n64-1)/7*0.4375 + 0.5625;
+  } else if (n64 >= 9 && n64 < 25) {
+    r = 0;
+    g = (n64-9)/15*0.9375 + 0.0625;
+    b = 1;
+  } else if (n64 >= 25 && n64 < 41) {
+    r = (n64-25)/15*0.9375 + 0.0625;
+    g = 1;
+    b = -(n64-25)/15*0.9375 + 0.9375;
+  } else if (n64 >= 41 && n64 < 57) {
+    r = 1;
+    g = -(n64-41)/15*0.9375 + 0.9375;
+    b = 0;
+  } else if (n64 >= 57 && n64 <= 64) {
+    r = -(n64-57)/7*0.4375 + 0.9375;
+    g = 0;
+    b = 0;
+  } else {
+    // out of bounds - give output for zero input
+    r = 0;
+    g = 0;
+    b = 0.5625;
+  } // end of IF structure
+  // but we must round output to integers after converting to 0-255
+  r = Math.round(r*255);
+  g = Math.round(g*255);
+  b = Math.round(b*255);
+  return [r,g,b];
+} // end of function jetColorMap
+
+function plotSpaceTimePlot() {
+  var canvas = document.getElementById('canvas_CANVAS_rate');
+  var context = canvas.getContext('2d');
+  // // test with example from
+  // //   https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial
+  // context.fillStyle = 'rgb(200,0,0)';
+  // context.fillRect(10,10,50,50);
+  // context.fillStyle = 'rgba(0,0,200,0.5)';
+  // context.fillRect(30,30,50,50);
+  // get data from array spaceTimeData and plot
+  var v = 0; // v = 0 is the index number of the variable to plot
+  var t;
+  var s;
+  var rate;
+  var r;
+  var g;
+  var b;
+  var jet;
+  var x;
+  var y;
+  // below we have to convert computed color values
+  // to text string for fillStyle below, so get pieces ready
+  var tColor1 = 'rgb(';
+  var tColor2;
+  var tColor3;
+  var tColor4;
+  var tColor5 = ')';
+  var tPixels = canvas.width;
+  var sPixels = canvas.height;
+  // numTimePts and numStripPts are globals defined above in this file
+  var tPixelsPerPoint = tPixels/(numTimePts+1); // pixels per point
+  var sPixelsPerPoint = sPixels/(numSpacePts+1); // pixels per point
+  var maxRate = 1/puCatalystLayer.model/puCatalystLayer.model; // 1/1/1 or 1/2/2
+  for (t = 0; t <= numTimePts; t += 1) { // NOTE = at t <=
+    for (s = 0; s <= numSpacePts; s += 1) { // NOTE = AT s <=
+      rate = spaceTimeData[v][t][s] / maxRate;
+      jet = jetColorMap(rate); // rate should be scaled 0 to 1
+      r = jet[0];
+      g = jet[1];
+      b = jet[2];
+      // we have to convert computed color values to string for fillStyle
+      tColor2 = r.toString();
+      tColor3 = g.toString();
+      tColor4 = b.toString();
+      tColor = tColor1.concat(tColor2,',',tColor3,',',tColor4,tColor5);
+      // document.getElementById("dev01").innerHTML = jetColorMap(1);
+      context.fillStyle = tColor;
+      // swap directions in plot from that in spaceTimeData array
+      x = tPixelsPerPoint * (numTimePts - t);
+      y = sPixelsPerPoint * (numSpacePts - s);
+      context.fillRect(x,y,tPixelsPerPoint,sPixelsPerPoint);
+    } // end of inner FOR repeat
+  } // end of outer FOR repeat
+} // end of function plotSpaceTimePlot
