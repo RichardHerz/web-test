88b6b9216c566cc219d63c30a3fdc2794895a8ae
RichardHerz <rich@reactorlab.net>
2018-10-09 18:56:02 -0700

    more work

--- p18/js/puCSTR.js
+++ p18/js/puCSTR.js
@@ -49,14 +49,14 @@ function puCSTR(pUnitIndex) {
   // colorCanvasData : [], // for color canvas plots, plot script requires this name
 
   // allow this unit to take more than one step within one main loop step in updateState method
-  this.unitStepRepeats = 100;
+  this.unitStepRepeats = 1;
   this.unitTimeStep = simParams.simTimeStep / this.unitStepRepeats;
 
   // define variables which will not be plotted nor saved in copy data table
   this.conc = 0;
 
   this.ssCheckSum = 0; // used to check for steady state
-  this.residenceTime = 0; // for timing checks for steady state check
+  this.residenceTime = 100; // for timing checks for steady state check
   // residenceTime is set in this unit's updateUIparams()
 
   this.initialize = function() {
@@ -112,39 +112,7 @@ function puCSTR(pUnitIndex) {
     // set to zero ssCheckSum used to check for steady state by this unit
     this.ssCheckSum = 0;
 
-    for (k = 0; k <= this.numNodes; k += 1) {
-      this.Ca[k] = this.dataInitial[4]; // [4] is Cain
-      this.Trxr[k] = this.dataInitial[6]; // [6] is TinHX
-    }
-
-    // initialize profile data array
-    // initPlotData(numProfileVars,numProfilePts)
-    this.profileData = plotter.initPlotData(2,this.numNodes); // holds data for static profile plots
-
-    // // initialize strip chart data array
-    // // initPlotData(numStripVars,numStripPts)
-    // this.stripData = plotter.initPlotData(numStripVars,numStripPts); // holds data for scrolling strip chart plots
-
-    // initialize local array to hold color-canvas data, e.g., space-time data -
-    // plotter.initColorCanvasArray(numVars,numXpts,numYpts)
-    // *** FOR ADIABATIC RXR, numVars = 1 since don't show jacket canvas
-    this.colorCanvasData = plotter.initColorCanvasArray(1,this.numNodes,1);
-
-    let kn = 0;
-    for (k = 0; k <= this.numNodes; k += 1) {
-      kn = k/this.numNodes;
-      // x-axis values
-      // x-axis values will not change during sim
-      // XXX change to get number vars for this plotInfo variable
-      //     so can put in repeat - or better yet, a function
-      //     and same for y-axis below
-      // first index specifies which variable
-      this.profileData[0][k][0] = kn;
-      this.profileData[1][k][0] = kn;
-      // y-axis values
-      this.profileData[0][k][1] = this.dataInitial[6]; // [6] is TinHX
-      this.profileData[1][k][1] = this.dataInitial[4]; // [4] is Cain
-    }
+    this.conc = 0;
 
     // update display
     this.updateDisplay();
@@ -187,7 +155,7 @@ function puCSTR(pUnitIndex) {
 
     // get array of current input values to this unit from other units
     let inputs = this.getInputs();
-    this.concIn = inputs[0]; // conc from upstream CSTR 
+    this.concIn = inputs[0]; // conc from upstream CSTR
 
   } // END of updateInputs()
 
@@ -201,54 +169,20 @@ function puCSTR(pUnitIndex) {
     // WARNING: this method must NOT contain references to other units!
     //          get info from other units ONLY in updateInputs() method
 
+    let flowrate = 1;
+    let volume = 100;
+    let krate = 0.01;
+
+    let dcdt = flowrate/volume * (this.concIn - this.comc) - krate * this.conc;
+
+    this.conc = this.conc + dcdt * this.unitTimeStep;
+
   } // END of updateState()
 
   this.updateDisplay = function() {
     // update display elements which only depend on this process unit
     // except do all plotting at main controller updateDisplay
     // since some plots may contain data from more than one process unit
-
-    // note use .toFixed(n) method of object to round number to n decimal points
-
-    let n = 0; // used as index
-
-    // document.getElementById(this.displayReactorLeftT).innerHTML = this.Tin.toFixed(1) + ' K';
-    document.getElementById(this.displayReactorLeftT).innerHTML = this.Trxr[0].toFixed(1) + ' K';
-    document.getElementById(this.displayReactorRightT).innerHTML = this.Trxr[this.numNodes].toFixed(1) + ' K';
-
-    // NOTE: HX cold out T (right) and RXR T in will not agree except at SS
-    // and HX hot in T (right) and RXR T out with not agree except at SS
-    // because RXR T in doesn't get set to HX cold out until start of next time step
-    // and HX hot in doesn't get set to RXR out until start of next time step
-    // decide not to force match in display so that display agrees with copy data
-
-    document.getElementById(this.displayReactorLeftConc).innerHTML = this.Cain.toFixed(1);
-    document.getElementById(this.displayReactorRightConc).innerHTML = this.Ca[this.numNodes].toFixed(1) + ' mol/m<sup>3</sup>';;
-
-    // HANDLE PROFILE PLOT DATA
-
-    // copy variable values to profileData array which holds data for plotting
-
-    // XXX CONSIDER RE-ORDERING LAST TWO INDEXES IN profileData SO CAN USE
-    //     SIMPLE ASSIGNMENT FOR ALL Y VALUES, e.g.,
-    // profileData[0][1][n] = y;
-
-    for (n=0; n<=this.numNodes; n+=1) {
-      this.profileData[0][n][1] = this.Trxr[n];
-      this.profileData[1][n][1] = this.Ca[n];
-    }
-
-    // HANDLE COLOR CANVAS DATA - HERE FOR PFR TEMPERATURE vs. POSITION
-    // the data vs. node is horizontal, not vertical
-    // and vertical strip is all the same
-    // so when initialize colorCanvasData array, take this into account
-    // FOR PFR - COLOR CANVAS DOES NOT SCROLL WITH TIME
-    // SO DO NOT SHIFT AND PUSH DATA LIKE DO IN SCROLLING CANVAS
-
-    // colorCanvasData[v][x][y]
-    for (n=0; n<=this.numNodes; n+=1) {
-      this.colorCanvasData[0][n][0] = this.Trxr[n];
-    }
 
   } // END of updateDisplay()
 
